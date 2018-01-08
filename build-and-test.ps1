dfe728b77b8dcd74b3489f3dbb38f50265c81970
Ravi Eda <raeda@microsoft.com>
2018-01-08 16:52:04 -0600

    Build and test images (#65)
    * Build and test images
    
    * Address PR feedback - 1
    
    * Address PR feedback - 2 (#2)
    
    * Address PR feedback - 3
    
    * Address PR feedback - 4
    
    * Remove astrix from versionList def

---
+++ build-and-test.ps1
@@ -0,0 +1,69 @@
+[cmdletbinding()]
+param(
+    [string]$VersionFilter = "*",
+    [string]$OSFilter = "*",
+    [string]$ImageBuilderCustomArgs
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$buildFilter = "$VersionFilter-$OSFilter/*"
+
+function ExecuteWithRetry {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Command
+    )
+
+    $attempt = 0
+    $maxRetries = 5
+    $waitFactor = 6
+    while ($attempt -lt $maxRetries) {
+        try {
+            Invoke-Expression -Command "$Command"
+            break
+        }
+        catch {
+            Write-Warning "$_"
+        }
+
+        $attempt++
+        if ($attempt -ne $maxRetries) {
+            $waitTime = $attempt * $waitFactor
+            Write-Host "Retry ${attempt}/${maxRetries} failed, retrying in $waitTime seconds..."
+            Start-Sleep -Seconds ($waitTime)
+            Remove-Item -Path "$imageBuilderFolder" -Recurse -Force -ErrorAction Continue
+        }
+        else {
+            throw "Retry ${attempt}/${maxRetries} failed, no more retries left."
+        }
+    }
+}
+
+function Get-ImageBuilder() {
+    $imageBuilderImageName = 'microsoft/dotnet-buildtools-prereqs:image-builder-nanoserver-20180105103709'
+    $imageBuilderContainerName = "ImageBuilder-$(Get-Date -Format yyyyMMddhhmmss)"
+    New-Item -Path "$imageBuilderFolder" -ItemType Directory -Force | Out-Null
+    ExecuteWithRetry -Command "docker pull $imageBuilderImageName"
+    Invoke-Expression "docker create --name $imageBuilderContainerName $imageBuilderImageName"
+    # Copying the 'image-builder' folder and not just the content due to https://github.com/moby/moby/issues/34638
+    Invoke-Expression "docker cp ${imageBuilderContainerName}:/image-builder $imageBuilderFolder"
+    Invoke-Expression "docker rmi -f $imageBuilderImageName"
+}
+
+$imageBuilderFolder = [System.IO.Path]::Combine("$PSScriptRoot", ".Microsoft.DotNet.ImageBuilder")
+$imageBuilderExe = [System.IO.Path]::Combine("$imageBuilderFolder", "image-builder", "Microsoft.DotNet.ImageBuilder.exe")
+if (-not (Test-Path -Path "$imageBuilderExe" -PathType Leaf)) {
+    Get-ImageBuilder
+}
+
+$imageBuilderArgs = "build --path $buildFilter --test-var VersionFilter=$VersionFilter --test-var OSFilter=$OSFilter"
+if (-not [string]::IsNullOrWhiteSpace($ImageBuilderCustomArgs)) {
+    $imageBuilderArgs += " $ImageBuilderCustomArgs"
+}
+
+Invoke-Expression "$imageBuilderExe $imageBuilderArgs"
+if ($LastExitCode -ne 0) {
+    throw "Failed executing $imageBuilderExe $imageBuilderArgs"
+}
---
+++ netci.groovy
@@ -0,0 +1,32 @@
+import jobs.generation.Utilities
+
+def project = GithubProject
+def branch = GithubBranchName
+def isPR = true
+def platformList = ['Windows_2016:WindowsServerCore-ltsc2016', 'Windows_2016:WindowsServerCore-1709']
+def versionList = ['3.5', '4.']
+
+platformList.each { platform ->
+    def(hostOS, containerOS) = platform.tokenize(':')
+    def machineLabel = 'latest-docker'
+
+    versionList.each { version ->
+        def newJobName = Utilities.getFullJobName(project, "${version}_${containerOS}", isPR)
+        def versionFilter = "${version}*"
+
+        def newJob = job(newJobName) {
+            steps { 
+                batchFile("powershell -NoProfile -Command .\\build-and-test.ps1 -VersionFilter \"${versionFilter}\" -OSFilter \"${containerOS}\"")
+            }
+        }
+
+        if (containerOS == 'WindowsServerCore-1709') {
+            newJob.with {label('windows.10.amd64.serverrs3.open')}
+        }
+        else {
+            Utilities.setMachineAffinity(newJob, hostOS, machineLabel)
+        }
+        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+        Utilities.addGithubPRTriggerForBranch(newJob, branch, "${containerOS} - ${version} Dockerfiles")
+    }
+}
