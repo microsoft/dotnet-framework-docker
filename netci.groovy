41af90530d40ecd5f5ea76f03fcefc385aed8a99
MichaelSimons <msimons@microsoft.com>
2018-12-06 13:40:36 -0600

    Enable CI for Windows 1809 images

--- netci.groovy
+++ netci.groovy
@@ -4,41 +4,38 @@ def project = GithubProject
 def branch = GithubBranchName
 def isPR = true
 def platformList = [
-        'Windows_2016:WindowsServerCore-ltsc2016',
-        'Windows_2016:WindowsServerCore-1709',
-        'Windows_2016:WindowsServerCore-1803'
-        //'Windows_2016:WindowsServerCore-ltsc2019'
+        'WindowsServerCore-ltsc2016',
+        'WindowsServerCore-1709',
+        'WindowsServerCore-1803',
+        'WindowsServerCore-ltsc2019'
     ]
 def versionList = ['3.5', '4.']
 
 platformList.each { platform ->
-    def(hostOS, containerOS) = platform.tokenize(':')
-    def machineLabel = 'latest-docker'
-
     versionList.each { version ->
-        def newJobName = Utilities.getFullJobName(project, "${version}_${containerOS}", isPR)
+        def newJobName = Utilities.getFullJobName(project, "${version}_${platform}", isPR)
         def versionFilter = "${version}*"
 
         def newJob = job(newJobName) {
             steps { 
-                batchFile("powershell -NoProfile -Command .\\build-and-test.ps1 -VersionFilter \"${versionFilter}\" -OSFilter \"${containerOS}\" -CleanupDocker")
+                batchFile("powershell -NoProfile -Command .\\build-and-test.ps1 -VersionFilter \"${versionFilter}\" -OSFilter \"${platform}\" -CleanupDocker")
             }
         }
 
-        if (containerOS == 'WindowsServerCore-ltsc2019') {
+        if (platform == 'WindowsServerCore-ltsc2019') {
             newJob.with {label('windows.10.amd64.serverrs5.open')}
         }
-        else if (containerOS == 'WindowsServerCore-1803') {
+        else if (platform == 'WindowsServerCore-1803') {
             newJob.with {label('windows.10.amd64.serverrs4.open')}
         }
-        else if (containerOS == 'WindowsServerCore-1709') {
+        else if (platform == 'WindowsServerCore-1709') {
             newJob.with {label('windows.10.amd64.serverrs3.open')}
         }
         else {
-            Utilities.setMachineAffinity(newJob, hostOS, machineLabel)
+            Utilities.setMachineAffinity(newJob, 'Windows_2016', 'latest-docker')
         }
         Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
         Utilities.setJobTimeout(newJob, 180)
-        Utilities.addGithubPRTriggerForBranch(newJob, branch, "${containerOS} - ${version} Dockerfiles")
+        Utilities.addGithubPRTriggerForBranch(newJob, branch, "${platform} - ${version} Dockerfiles")
     }
 }
