8e262a0a74177c35152679e87f6db62bbb3dfd3d
MichaelSimons <msimons@microsoft.com>
2018-12-12 16:03:19 -0600

    Update Dockerfile link pattern used in readme tags section

--- scripts/Get-TagsDocumentation.ps1
+++ scripts/Get-TagsDocumentation.ps1
@@ -1,17 +1,38 @@
 param(
     [string]$Branch='master',
-    [string]$Manifest='manifest.json',
-    [string]$ImageBuilderImageName='microsoft/dotnet-buildtools-prereqs:image-builder-debian-20180522205353'
+    [string]$ImageBuilderImageName='microsoft/dotnet-buildtools-prereqs:image-builder-debian-20181030184558'
 )
 
 $ErrorActionPreference = 'Stop'
 $repoRoot = Split-Path -Path "$PSScriptRoot" -Parent
 
+function GenerateDoc {
+    param ([string] $Manifest, [string] $Repo, [string] $ReadmePath, [string] $Template)
+
+    & docker run --rm `
+        -v /var/run/docker.sock:/var/run/docker.sock `
+        -v "${repoRoot}:/repo" `
+        -w /repo `
+        $ImageBuilderImageName `
+            generateTagsReadme `
+            --manifest $Manifest `
+            --repo $Repo `
+            --update-readme `
+            --readme-path $ReadmePath `
+            --template $Template `
+            "https://github.com/Microsoft/dotnet-framework-docker/blob/${Branch}"
+}
+
 & docker pull $ImageBuilderImageName
 
-& docker run --rm `
-    -v /var/run/docker.sock:/var/run/docker.sock `
-    -v "${repoRoot}:/repo" `
-    -w /repo `
-    $ImageBuilderImageName `
-    generateTagsReadme --update-readme --manifest $Manifest "https://github.com/Microsoft/dotnet-framework-docker/blob/${Branch}"
+GenerateDoc `
+    -Manifest manifest.json `
+    -Repo microsoft/dotnet-framework `
+    -ReadmePath ./README.md `
+    -Template ./scripts/ReadmeTagsDocumentationTemplate.md
+
+GenerateDoc `
+    -Manifest manifest.samples.json `
+    -Repo microsoft/dotnet-framework-samples `
+    -ReadmePath ./samples/README.DockerHub.md `
+    -Template ./scripts/SamplesReadmeTagsDocumentationTemplate.md
