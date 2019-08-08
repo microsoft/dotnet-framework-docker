4fba36493953ae5f4836f96a536aa21e3ef50b43
Hong Li <hongli@microsoft.com>
2019-08-08 14:53:25 -0700

    Update tests/Microsoft.DotNet.Framework.Docker.Tests/ImageTests.cs
    Co-Authored-By: Michael Simons <msimons@microsoft.com>

--- tests/Microsoft.DotNet.Framework.Docker.Tests/ImageTests.cs
+++ tests/Microsoft.DotNet.Framework.Docker.Tests/ImageTests.cs
@@ -61,7 +61,7 @@ namespace Microsoft.DotNet.Framework.Docker.Tests
                 new ImageDescriptor { RuntimeVersion = "4.7.2", OsVariant = WSC_LTSC2016 },
                 new ImageDescriptor { RuntimeVersion = "4.7.2", OsVariant = WSC_1803 },
                 new ImageDescriptor { RuntimeVersion = "4.7.2", OsVariant = WSC_LTSC2019 },
-                new ImageDescriptor { RuntimeVersion = "4.7.2", OsVariant = WSC_LTSC2019 },
+                new ImageDescriptor { RuntimeVersion = "4.7.2", OsVariant = WSC_1903 },
                 new ImageDescriptor { RuntimeVersion = "4.8", OsVariant = WSC_1803 },
                 new ImageDescriptor { RuntimeVersion = "4.8", OsVariant = WSC_LTSC2016 },
                 new ImageDescriptor { RuntimeVersion = "4.8", OsVariant = WSC_LTSC2019 },
