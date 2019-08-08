9f87d4655687b7aa68ef3263ab808544be75aa79
Michael Simons <msimons@microsoft.com>
2019-08-08 14:40:34 -0500

    Fix broken Dockerfile links in the readmes (#344)

--- eng/Get-TagsDocumentation.ps1
+++ eng/Get-TagsDocumentation.ps1
@@ -5,4 +5,5 @@ $gitRepo = "https://github.com/microsoft/dotnet-framework-docker"
 
 & $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/framework/runtime README.runtime.md manifest.json master $gitRepo
 & $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/framework/sdk README.sdk.md manifest.json master $gitRepo -ReuseImageBuilderImage
+& $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/framework/wcf README.wcf.md manifest.wcf.json master $gitRepo
 & $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/framework/samples README.samples.md manifest.samples.json master $gitRepo -ReuseImageBuilderImage
